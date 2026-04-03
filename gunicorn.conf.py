bb36c5d88056d231666f9b5d54635d064939e714
trav <travtherobber@gmail.com>
2026-04-02 21:12:08 -0400

    render fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,8 +2,7 @@ import os
 
 
 bind = f"0.0.0.0:{os.getenv('PORT', '10000')}"
-workers = int(os.getenv("WEB_CONCURRENCY", "2"))
-threads = int(os.getenv("GUNICORN_THREADS", "4"))
-timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
+workers = int(os.getenv("WEB_CONCURRENCY", "1"))
+threads = int(os.getenv("GUNICORN_THREADS", "2"))
+timeout = int(os.getenv("GUNICORN_TIMEOUT", "180"))
 worker_tmp_dir = "/tmp"
-
